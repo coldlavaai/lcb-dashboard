4a237a611dcd83f645c801f5df58dc6e3d84dad1
coldlavaai <otatler@gmail.com>
2025-10-30 20:05:53 +0000

    Fix: Remove @sanity/vision dependency

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './sanity/schemaTypes'
 
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   projectId: 'YOUR_PROJECT_ID', // You'll need to provide this
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
